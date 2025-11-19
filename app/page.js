63cb3a96a9e100befa7d72c10adb6f5a30c8477b
aqusharhussain007-crypto <aqusharhussain007@gmail.com>
2025-11-19 14:02:59 +0530

    Display sample products on Home page
    Added sample product listings to the Home component.

--- app/page.js
+++ app/page.js
@@ -1,4 +1,27 @@
+import ProductCard from "./ProductCard";
+
 export default function Home() {
+  const sampleProducts = [
+    {
+      store: "Amazon",
+      name: "Wireless Earbuds",
+      price: 1299,
+      link: "https://www.amazon.in/",
+    },
+    {
+      store: "Flipkart",
+      name: "Smartwatch",
+      price: 2199,
+      link: "https://www.flipkart.com/",
+    },
+    {
+      store: "Meesho",
+      name: "Backpack",
+      price: 899,
+      link: "https://www.meesho.com/",
+    },
+  ];
+
   return (
     <div style={{ padding: "40px", fontFamily: "Arial" }}>
       <h1>Welcome to OodlesNet</h1>
@@ -18,10 +41,25 @@ export default function Home() {
         />
       </div>
 
-      <p style={{ marginTop: "20px" }}>
-        Product listings will appear here soon…
-      </p>
+      <h2 style={{ marginTop: "30px" }}>Sample Products</h2>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          marginTop: "15px",
+        }}
+      >
+        {sampleProducts.map((product, index) => (
+          <ProductCard
+            key={index}
+            store={product.store}
+            name={product.name}
+            price={product.price}
+            link={product.link}
+          />
+        ))}
+      </div>
     </div>
   );
-                  }
-          
+            }
+  
