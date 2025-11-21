eaec13cf3190a2458b2d6575eb2649fa794a12e6
aqusharhussain007-crypto <aqusharhussain007@gmail.com>
2025-11-21 19:26:56 +0530

    Remove app-body class from body element

--- app/layout.js
+++ app/layout.js
@@ -7,9 +7,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="app-body">
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
