c5809cb2bc03ac3c80d52e2552a29b0612e7a2e1
aqusharhussain007-crypto <aqusharhussain007@gmail.com>
2025-11-24 15:20:56 +0530

    Delete data/products.js

--- data/products.js
+++
@@ -1,9 +0,0 @@
-export const products = [
-  {
-    id: "1",
-    name: "Sample Product",
-    price: "₹999",
-    image: "https://via.placeholder.com/300",
-    description: "A sample product used for testing."
-  }
-];
