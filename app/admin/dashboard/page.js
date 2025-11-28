758f00e3cae80ea7ce5fd69a8a4451f074cd8661
aqusharhussain007-crypto <aqusharhussain007@gmail.com>
2025-11-28 14:58:52 +0530

    Update page.js
    Fix: final cleanup for AdminLoginView & AdminDashboardView

--- app/admin/dashboard/page.js
+++ app/admin/dashboard/page.js
@@ -1,1 +1,10 @@
+"use client";
+import dynamic from "next/dynamic";
 
+const Dashboard = dynamic(() => import("@/components/AdminDashboardView"), {
+  ssr: false,
+});
+
+export default function Page() {
+  return <Dashboard />;
+    }
