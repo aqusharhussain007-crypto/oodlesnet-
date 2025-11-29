d98cea18db14972456598058279c82935340744a
aqusharhussain007-crypto <aqusharhussain007@gmail.com>
2025-11-29 21:08:03 +0530

    Update page.js

--- app/admin/login/page.js
+++ app/admin/login/page.js
@@ -1,10 +1,9 @@
-"use client";
-
 import dynamic from "next/dynamic";
 
-const AdminLoginView = dynamic(() => import("@/components/AdminLoginView"), {
-  ssr: false,
-});
+const AdminLoginView = dynamic(
+  () => import("@/components/AdminLoginView"),
+  { ssr: false }
+);
 
 export default function Page() {
   return <AdminLoginView />;
