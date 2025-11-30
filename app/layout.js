c4ba6200d033e31d5dd1b188f88dd9ed64b66a2d
aqusharhussain007-crypto <aqusharhussain007@gmail.com>
2025-11-30 14:28:53 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,15 +4,25 @@ import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "OodlesNet",
-  description: "Modern product comparison website",
+  description: "Smart price comparison website",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body
+        style={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          background: "#e4e9f1",
+        }}
+      >
         <Navbar />
-        {children}
+
+        {/* Main Content Pushes Footer Down */}
+        <main style={{ flex: 1 }}>{children}</main>
+
         <Footer />
       </body>
     </html>
