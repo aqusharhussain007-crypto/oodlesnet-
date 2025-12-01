ebf7ee7f20f2ab2ef4af80533c23133f83ef33ee
aqusharhussain007-crypto <aqusharhussain007@gmail.com>
2025-12-01 17:04:11 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -27,8 +27,8 @@ export default function Navbar() {
           src="/logo.png"
           alt="logo"
           style={{
-            height: "120px",
-            width: "auto",
+            height: "80px",
+            width: "180",
             objectFit: "contain",
             cursor: "pointer",
             display: "block",
