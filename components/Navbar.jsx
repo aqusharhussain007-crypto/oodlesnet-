b4e0d3e0deca64e7fee6cd0270809cab4b9bae79
aqusharhussain007-crypto <aqusharhussain007@gmail.com>
2025-12-03 17:09:11 +0530

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
       style={{
         width: "100%",
         background: "#071018",
-        padding: "6px 14px",
+        padding: "4px 12px",
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
