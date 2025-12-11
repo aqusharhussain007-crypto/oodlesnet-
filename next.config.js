d122d09c973e681aa48194b212845be466bfc981
aqusharhussain007-crypto <aqusharhussain007@gmail.com>
2025-12-11 09:36:00 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",   // allow ALL https image sources
+      },
+      {
+        protocol: "http",
+        hostname: "**",   // allow http images if any
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
