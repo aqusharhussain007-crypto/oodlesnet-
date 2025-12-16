81eb492ee0bb21b0deaaa936efd5d210dfa345fe
aqusharhussain007-crypto <aqusharhussain007@gmail.com>
2025-12-16 22:09:47 +0530

    Update layout.js

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,13 +1,17 @@
 import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 
+export function generateStaticParams() {
+  return [{ locale: "en" }, { locale: "hi" }];
+}
+
 export default async function LocaleLayout({ children, params }) {
   const { locale } = params;
 
   let messages;
   try {
     messages = (await import(`../../messages/${locale}.json`)).default;
-  } catch (error) {
+  } catch {
     notFound();
   }
 
@@ -20,4 +24,5 @@ export default async function LocaleLayout({ children, params }) {
       </body>
     </html>
   );
-}
+    }
+    
