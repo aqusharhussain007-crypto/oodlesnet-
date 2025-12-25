b6ca1d5b4b7ebba6f49b194bb1c1901daec1a524
aqusharhussain007-crypto <aqusharhussain007@gmail.com>
2025-12-25 16:19:44 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,42 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  images: {
-    remotePatterns: [
-      // Amazon CDN
-      {
-        protocol: "https",
-        hostname: "m.media-amazon.com",
-      },
-
-      // Meesho / Ajio / Flipkart / generic CDNs
-      {
-        protocol: "https",
-        hostname: "*.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "*.cloudfront.net",
-      },
-
-      // Stock image providers (already used)
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-
-      // Any future CDN you may add
-      {
-        protocol: "https",
-        hostname: "*.googleusercontent.com",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
