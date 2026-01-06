f0cdd851266c44c023340100004306b0ca76ebbe
aqusharhussain007-crypto <aqusharhussain007@gmail.com>
2026-01-06 12:05:48 +0530

    Create uploadProducts.js

---
+++ scripts/uploadProducts.js
@@ -0,0 +1,123 @@
+[
+  {
+    "name": "Apple iPhone 15",
+    "brand": "Apple",
+    "categorySlug": "mobile",
+    "description": "Apple iPhone 15 with A16 Bionic chip, advanced dual-camera system and premium design.",
+    "imageUrl": "https://m.media-amazon.com/images/I/71d7rfSl0wL._SX679_.jpg",
+    "impressions": 0,
+    "store": [
+      { "name": "Amazon", "price": 79999, "offer": "No Cost EMI", "url": "https%3A%2F%2Fwww.amazon.in%2Fdp%2FB0CHX2F5QT" },
+      { "name": "Flipkart", "price": 78999, "offer": "Bank Offer", "url": "https%3A%2F%2Fwww.flipkart.com%2Fapple-iphone-15" }
+    ]
+  },
+  {
+    "name": "Samsung Galaxy S23 FE",
+    "brand": "Samsung",
+    "categorySlug": "mobile",
+    "description": "Samsung Galaxy S23 FE with Exynos processor, AMOLED display and flagship-grade camera.",
+    "imageUrl": "https://m.media-amazon.com/images/I/61-RZQGmJqL._SX679_.jpg",
+    "impressions": 0,
+    "store": [
+      { "name": "Amazon", "price": 39999, "offer": "Exchange Offer", "url": "https%3A%2F%2Fwww.amazon.in%2Fdp%2FB0CGTQXJH1" },
+      { "name": "Flipkart", "price": 38999, "offer": "Bank Discount", "url": "https%3A%2F%2Fwww.flipkart.com%2Fsamsung-galaxy-s23-fe" }
+    ]
+  },
+  {
+    "name": "OnePlus Nord CE 3 Lite 5G",
+    "brand": "OnePlus",
+    "categorySlug": "mobile",
+    "description": "OnePlus Nord CE 3 Lite with 5G connectivity, 108MP camera and fast charging.",
+    "imageUrl": "https://m.media-amazon.com/images/I/61LB+d0vheL._SX679_.jpg",
+    "impressions": 0,
+    "store": [
+      { "name": "Amazon", "price": 19999, "offer": "No Cost EMI", "url": "https%3A%2F%2Fwww.amazon.in%2Fdp%2FB0BZZX4J7K" },
+      { "name": "Flipkart", "price": 19499, "offer": "Exchange Offer", "url": "https%3A%2F%2Fwww.flipkart.com%2Foneplus-nord-ce-3-lite-5g" }
+    ]
+  },
+  {
+    "name": "Redmi Note 13 Pro 5G",
+    "brand": "Xiaomi",
+    "categorySlug": "mobile",
+    "description": "Redmi Note 13 Pro with curved AMOLED display, 200MP camera and fast charging.",
+    "imageUrl": "https://m.media-amazon.com/images/I/71lZp3gC5mL._SX679_.jpg",
+    "impressions": 0,
+    "store": [
+      { "name": "Amazon", "price": 25999, "offer": "Bank Offer", "url": "https%3A%2F%2Fwww.amazon.in%2Fdp%2FB0CRH9H7TZ" },
+      { "name": "Flipkart", "price": 25499, "offer": "No Cost EMI", "url": "https%3A%2F%2Fwww.flipkart.com%2Fredmi-note-13-pro-5g" }
+    ]
+  },
+  {
+    "name": "Realme Narzo 60 5G",
+    "brand": "Realme",
+    "categorySlug": "mobile",
+    "description": "Realme Narzo 60 with slim design, AMOLED display and reliable 5G performance.",
+    "imageUrl": "https://m.media-amazon.com/images/I/71Zf9uUp+GL._SX679_.jpg",
+    "impressions": 0,
+    "store": [
+      { "name": "Amazon", "price": 17999, "offer": "Limited Deal", "url": "https%3A%2F%2Fwww.amazon.in%2Fdp%2FB0C8JZ6Q6R" },
+      { "name": "Flipkart", "price": 17499, "offer": "Bank Discount", "url": "https%3A%2F%2Fwww.flipkart.com%2Frealme-narzo-60-5g" }
+    ]
+  },
+  {
+    "name": "iQOO Z7 Pro 5G",
+    "brand": "iQOO",
+    "categorySlug": "mobile",
+    "description": "iQOO Z7 Pro with curved AMOLED display and Dimensity processor.",
+    "imageUrl": "https://m.media-amazon.com/images/I/61Id6WJDWqL._SX679_.jpg",
+    "impressions": 0,
+    "store": [
+      { "name": "Amazon", "price": 23999, "offer": "No Cost EMI", "url": "https%3A%2F%2Fwww.amazon.in%2Fdp%2FB0C9Q6W1J5" },
+      { "name": "Flipkart", "price": 22999, "offer": "Exchange Offer", "url": "https%3A%2F%2Fwww.flipkart.com%2Fiqoo-z7-pro-5g" }
+    ]
+  },
+  {
+    "name": "Motorola Edge 40",
+    "brand": "Motorola",
+    "categorySlug": "mobile",
+    "description": "Motorola Edge 40 with clean Android, curved OLED display and fast charging.",
+    "imageUrl": "https://m.media-amazon.com/images/I/71cJ1y5x0UL._SX679_.jpg",
+    "impressions": 0,
+    "store": [
+      { "name": "Amazon", "price": 24999, "offer": "Bank Offer", "url": "https%3A%2F%2Fwww.amazon.in%2Fdp%2FB0C3Z9N9K3" },
+      { "name": "Flipkart", "price": 24499, "offer": "No Cost EMI", "url": "https%3A%2F%2Fwww.flipkart.com%2Fmotorola-edge-40" }
+    ]
+  },
+  {
+    "name": "Samsung Galaxy M34 5G",
+    "brand": "Samsung",
+    "categorySlug": "mobile",
+    "description": "Samsung Galaxy M34 with Super AMOLED display and long-lasting battery.",
+    "imageUrl": "https://m.media-amazon.com/images/I/71kDUwLtlPL._SX679_.jpg",
+    "impressions": 0,
+    "store": [
+      { "name": "Amazon", "price": 16999, "offer": "Limited Deal", "url": "https%3A%2F%2Fwww.amazon.in%2Fdp%2FB0C9F6M7WZ" },
+      { "name": "Flipkart", "price": 16499, "offer": "Bank Discount", "url": "https%3A%2F%2Fwww.flipkart.com%2Fsamsung-galaxy-m34-5g" }
+    ]
+  },
+  {
+    "name": "Vivo T2 Pro 5G",
+    "brand": "Vivo",
+    "categorySlug": "mobile",
+    "description": "Vivo T2 Pro with curved AMOLED display and fast charging.",
+    "imageUrl": "https://m.media-amazon.com/images/I/71b8oGZ+5SL._SX679_.jpg",
+    "impressions": 0,
+    "store": [
+      { "name": "Amazon", "price": 23999, "offer": "No Cost EMI", "url": "https%3A%2F%2Fwww.amazon.in%2Fdp%2FB0C9W4YJ7Z" },
+      { "name": "Flipkart", "price": 22999, "offer": "Exchange Offer", "url": "https%3A%2F%2Fwww.flipkart.com%2Fvivo-t2-pro-5g" }
+    ]
+  },
+  {
+    "name": "Poco X6 Pro 5G",
+    "brand": "Poco",
+    "categorySlug": "mobile",
+    "description": "Poco X6 Pro with powerful Dimensity chipset and AMOLED display.",
+    "imageUrl": "https://m.media-amazon.com/images/I/71x0C7U0ZGL._SX679_.jpg",
+    "impressions": 0,
+    "store": [
+      { "name": "Amazon", "price": 26999, "offer": "Bank Offer", "url": "https%3A%2F%2Fwww.amazon.in%2Fdp%2FB0CRB5P2S3" },
+      { "name": "Flipkart", "price": 26499, "offer": "No Cost EMI", "url": "https%3A%2F%2Fwww.flipkart.com%2Fpoco-x6-pro-5g" }
+    ]
+  }
+]
+    
